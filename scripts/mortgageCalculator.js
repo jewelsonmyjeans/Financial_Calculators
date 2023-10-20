e20c349ebeb034440da37a525ba821faed6fbc0f
Aviad Churaman <aviadchuraman@gmail.com>
2023-10-20 17:24:24 -0400

    finished Mortgage Calculator

--- scripts/mortgageCalculator.js
+++ scripts/mortgageCalculator.js
@@ -1,4 +1,30 @@
-const homePrice = document.getElementById("home-price")
-const downPayment = document.getElementById("down-payment")
-const loanTerm = document.getElementById("loan-term")
-const interestRate = document.getElementById("interest-rate")
+const calculateBtn = document.getElementById('calculate-btn')
+
+calculateBtn.addEventListener("click", function() {
+    calculateMonthlyPayment()
+})
+
+function calculateMonthlyPayment() {
+    const homePrice = parseFloat(document.getElementById("home-price").value)
+    const downPayment = parseFloat(document.getElementById("down-payment").value)
+    const loanTerm = parseFloat(document.getElementById("loan-term").value)
+    const interestRate = parseFloat(document.getElementById("interest-rate").value) / 100
+
+    const principal = homePrice - downPayment
+    const monthlyInterestRate = interestRate / 12
+    const numberOfPayments = loanTerm * 12
+
+    if (interestRate === 0) {
+        const monthlyPayment = principal / numberOfPayments
+        displayMonthlyPayment(monthlyPayment)
+    } else {
+        const monthlyPayment = (principal * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -numberOfPayments))
+        displayMonthlyPayment(monthlyPayment)
+    }
+
+    function displayMonthlyPayment(monthlyPayment) {
+        const resultElement = document.getElementById("monthly-payment-result")
+        resultElement.textContent = `Monthly Payment $${monthlyPayment.toFixed(2)}`
+    }
+
+}
