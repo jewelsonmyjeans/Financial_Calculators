1622ffd8409408e994d9ca38d9085dddbea378ac
Aviad Churaman <aviadchuraman@gmail.com>
2023-10-20 17:05:35 -0400

    added JS files, added variables for mortgageCalculator.js, and finished structure for mortgage.html

---
+++ scripts/mortgageCalculator.js
@@ -0,0 +1,4 @@
+const homePrice = document.getElementById("home-price")
+const downPayment = document.getElementById("down-payment")
+const loanTerm = document.getElementById("loan-term")
+const interestRate = document.getElementById("interest-rate")
